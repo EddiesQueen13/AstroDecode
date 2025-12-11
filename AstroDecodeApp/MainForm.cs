9e001ac118faf5f5ac43512a2379601e0ef10556
EddiesQueen13 <ashley.cochran0719@gmail.com>
2025-12-11 07:09:39 -0500

    Add MainForm class for AstroDecodeApp

---
+++ AstroDecodeApp/MainForm.cs
@@ -0,0 +1,14 @@
+using System.Windows.Forms;
+
+namespace AstroDecodeApp
+{
+    public class MainForm : Form
+    {
+        public MainForm()
+        {
+            this.Text = "Astro Decode";
+            this.Width = 800;
+            this.Height = 600;
+        }
+    }
+}
